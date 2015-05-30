6a27d08cb918472ff70abe28c1e1f3e783b43930
Vivek Sampara <ravensnowbird@gmail.com>
2015-05-31 04:11:35 +0530

    added fieldset for notifiers

--- app/models/ngo.rb
+++ app/models/ngo.rb
@@ -2,7 +2,8 @@ require 'region_helper'
 class Ngo < ActiveRecord::Base
 	#has_many :regions, -> {where :region_for => 'Ngo'}, :foreign_key => 'region_for_id'
 	include RegionHelper
-	has_many :notifiers
+	has_many :notifiers, :dependent => :destroy
+	accepts_nested_attributes_for :notifiers, :reject_if => lambda { |a| a[:email].blank? }, :allow_destroy => true
 	after_create :create_notifier
 
 
