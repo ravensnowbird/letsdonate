4e8c20da73e0acd573a6d3f65593b934e97204ab
rafeeq <rafeeq.sk786@gmail.com>
2015-05-30 19:47:15 +0530

    added region helpor module

--- app/models/ngo.rb
+++ app/models/ngo.rb
@@ -1,4 +1,5 @@
 class Ngo < ActiveRecord::Base
-	has_many :regions, -> {where :region_for => 'Ngo'}, :foreign_key => 'region_for_id'
+	#has_many :regions, -> {where :region_for => 'Ngo'}, :foreign_key => 'region_for_id'
+	include RegionHelper
 	has_many :notifiers
 end
--- app/models/transporter.rb
+++ app/models/transporter.rb
@@ -1,3 +1,4 @@
 class Transporter < ActiveRecord::Base
-	has_many :regions, -> {where :region_for => 'Transporter'}, :foreign_key => 'region_for_id'
+	#has_many :regions, -> {where :region_for => 'Transporter'}, :foreign_key => 'region_for_id'
+	include RegionHelper
 end
