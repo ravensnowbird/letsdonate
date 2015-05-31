6339b6730deaf8120d8650b7580f9e38956e7669
Vivek Sampara <ravensnowbird@gmail.com>
2015-05-31 13:52:10 +0530

    minor fixes2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_filter :trans , :only => [:dashboard]
+
   def set_user_type_form
   end
 
@@ -10,13 +13,14 @@ class UsersController < ApplicationController
     @transporter = Transporter.where(:user_id => current_user.id, :email => current_user.email).first
     @ngo = Ngo.find_by(:user_id => current_user.id)
     if current_user.has_role?
-      redirect_to user_dashboard_path 
+      redirect_to user_dashboard_path
     end
   end
 
   def dashboard
+
     @region = Region.new
-    @transporter = Transporter.where(:email => current_user.email).first 
+    @transporter = Transporter.where(:email => current_user.email).first
     @added_regions = @transporter.regions
   end
 
@@ -24,5 +28,9 @@ class UsersController < ApplicationController
     @region = Region.new
     @ngo = Notifier.where(:email => current_user.email).collect{|x| x.ngo}.first
     @added_regions = @ngo.regions
+  end
+
+  def trans
+    redirect_to "/transporters/new" if Transporter.where(:email => current_user.email).blank?
   end
 end
